2a231f4a8a0a9fb72176178596374ea07d0b0e33
Nick Jantz <thenickjantz@gmail.com>
2023-10-10 20:58:18 -0500

    update to attach cookie to request on login

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, jsonify, render_template, redirect
 from dotenv import load_dotenv
 from supabase import create_client, Client
+from functools import wraps
 
 load_dotenv()
 
@@ -11,6 +12,19 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
+def get_user_data(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        jwt = request.cookies.get('auth') or ''
+        if not jwt:
+            return redirect('/login')
+        try:
+            supabase_user = supabase.auth.get_user(jwt)
+        except Exception as e:
+            print(f"Exception: {e}")
+        return f(supabase_user.user, *args, **kwargs)
+    return decorated_function
+
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
@@ -19,15 +33,24 @@ def login():
 def supabase_login():
     email = request.form.get('email')
     password = request.form.get('password')
-    supabase.auth.sign_in_with_password({'email': email, "password": password})
-    return redirect("/user_profile")
+    user = supabase.auth.sign_in_with_password({'email': email, "password": password})
+    response = redirect("/user_profile")
+    response.set_cookie('auth', user.session.access_token)
+    supabase.auth.sign_out()
+    return response
+
+@app.route('/supabase/logout', methods=['POST'])
+def supabase_logout():
+    response = redirect("/login")
+    response.set_cookie('auth', expires=0)
+    supabase.auth.sign_out()
+    return response
 
 @app.route('/user_profile', methods=['GET'])
-def user_profile():
-    supabase_user = supabase.auth.get_user()
-    print('supabase user:')
+@get_user_data
+def user_profile(supabase_user):
     print(supabase_user)
-    return 'hello'
+    return render_template('user_profile.html', email=supabase_user.email, metadata=supabase_user.user_metadata)
 
 if __name__ == '__main__':
     app.run(debug=True)
